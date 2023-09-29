f28a149c1302ed8912268fe5a4decf4c22a7e06b
Poojanpatel2003 <poojanpatel1595@gmail.com>
2023-09-29 23:19:34 +0530

    first commit

---
+++ main.js
@@ -0,0 +1,7 @@
+let typed = new Typed(".text", {
+    strings: ["Student from Parul University", "frontend developer", "Web developer"],
+    typeSpeed: 120,
+    backSpeed: 150,
+    backDelay: 1600,
+    loop: true
+});
